4b4e81ef569f896edd39ed19d1b74252e45ffb2f
harkairt <32367206+harkairt@users.noreply.github.com>
2022-06-18 13:49:54 +0200

    base

---
+++ android/app/src/main/kotlin/com/example/go_initial_location/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.go_initial_location
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+void main() => runApp(const App());
+
+class App extends StatefulWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  late GoRouter router;
+
+  @override
+  void initState() {
+    router = GoRouter(
+      initialLocation: '/auth/login',
+      debugLogDiagnostics: true,
+      routes: [
+        GoRoute(
+          path: '/',
+          redirect: (state) => "/auth",
+        ),
+        GoRoute(
+          path: '/auth',
+          builder: (context, state) => const Page(title: '/auth'),
+          routes: [
+            GoRoute(
+              path: 'login',
+              builder: (context, state) => const Page(title: '/auth/login'),
+            ),
+          ],
+        ),
+        GoRoute(
+          path: '/contact',
+          builder: (context, state) => const Page(title: '/contact'),
+        ),
+      ],
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerDelegate: router.routerDelegate,
+      routeInformationParser: router.routeInformationParser,
+    );
+  }
+}
+
+class Page extends StatelessWidget {
+  const Page({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(title),
+      ),
+    );
+  }
+}
